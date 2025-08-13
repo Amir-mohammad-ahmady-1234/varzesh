4f656a1908b6379d9407f0771d66ab689773cd19
Mahdi <mahdibagherichanel@gmail.com>
2025-08-13 14:04:15 +0330

    Add the design of the system for buttons and improved style

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 function page() {
-  return <div>page</div>;
+  return (
+    <>
+      <button className="button">
+        <p className="text-2xl">sadas</p>
+      </button>
+      <button className="button-primary button">rf</button>
+      <button className="button-secondary button"> sdf</button>
+    </>
+  );
 }
 
 export default page;
