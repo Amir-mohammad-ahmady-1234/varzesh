0cc032aff877e6fd356c6d065692dece2d493617
Mahdi <mahdibagherichanel@gmail.com>
2025-08-16 00:58:58 +0330

    feat: add OTP generation and expiration check

--- app/api/auth/resereq/route.ts
+++ app/api/auth/resereq/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import prisma from "../../../../lib/db";
+import { authenticator } from "otplib";
 
 export async function POST(req: Request) {
   const { phone } = await req.json();
@@ -10,4 +11,24 @@ export async function POST(req: Request) {
   if (!existphone) {
     return NextResponse.json({ message: "User has already registered " });
   }
+  if (
+    existphone.otpExpiresAt &&
+    new Date() < new Date(existphone.otpExpiresAt)
+  ) {
+    return NextResponse.json({ message: "OTP هنوز معتبر است" });
+  }
+  const secret = authenticator.generateSecret();
+  const otp = authenticator.generate(secret);
+
+  // 5 دقیقه ممد
+  const otpExpiresAt = new Date(Date.now() + 5 * 60 * 1000);
+  await prisma.user.update({
+    where: { phone },
+    data: {
+      otpExpiresAt,
+      otpCode: otp,
+    },
+  });
+
+  return NextResponse.json({ ok: true });
 }
