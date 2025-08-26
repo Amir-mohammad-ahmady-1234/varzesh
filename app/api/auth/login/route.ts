148a4105e0920f5c1d89b435bdf18ac678834636
Amir-Mohammad <amyrmhmdahmdy179@gmail.com>
2025-08-25 19:59:51 -0700

    start get user details

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import prisma from "../../../../lib/db";
+import { LoginSchema } from "../../../../lib/validations/auth";
 
 export async function POST(req: Request) {
   try {
--- components/site/layout/header/Auth-Or-Profile-btn/ProfileButton.tsx
+++ components/site/layout/header/Auth-Or-Profile-btn/ProfileButton.tsx
@@ -1,5 +1,9 @@
+import GetUserById from "../../../../../server/admin/GetAdminById/GetAdminById";
 import Button from "../../../../../styles/ui/Button";
 
-export function ProfileButton() {
+export async function ProfileButton() {
+  const userID = await GetUserById();
+  console.log(userID);
+
   return <Button>profile</Button>;
 }
