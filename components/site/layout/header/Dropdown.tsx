73d969c37abfe7778c0f636afd03bd03eb38a6f0
Mahdi <mahdibagherichanel@gmail.com>
2025-08-28 00:13:10 +0330

    add Dropdown

---
+++ components/site/layout/header/Dropdown.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import React, { useState } from "react";
+import DropdownMenuHeader from "./DropdownMenuHeader";
+import Button from "../../../../styles/ui/Button";
+import { MdOutlineKeyboardArrowDown } from "react-icons/md";
+
+interface Props {
+  DropItems: {
+    id: number;
+    name: string;
+    dropdown: {
+      id: number;
+      name: string;
+    }[];
+  }[];
+  usage?: string;
+}
+
+function Dropdown({ DropItems, usage = "header" }: Props) {
+  const [openDropdown, setOpenDropdown] = useState<number | null>(null);
+
+  return (
+    <nav className="hidden md:flex items-center gap-6 ">
+      {DropItems?.map((item) => (
+        <div
+          key={item.id}
+          className="relative"
+          onMouseEnter={() => setOpenDropdown(item.id)}
+          onMouseLeave={() => setOpenDropdown(null)}
+        >
+          <DropdownMenuHeader item={item} isOpen={openDropdown === item.id} />
+          <Button
+            variant={usage === "header" ? "ghost" : "primary"}
+            size="md"
+            className="px-3 py-2 text-neutral-50 hover:text-primary-500 transition-colors flex items-center gap-2"
+          >
+            {item.name}
+            <MdOutlineKeyboardArrowDown />
+          </Button>
+        </div>
+      ))}
+    </nav>
+  );
+}
+
+export default Dropdown;
