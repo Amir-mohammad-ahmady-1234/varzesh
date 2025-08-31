39e77476bfc8107e85a51c587113e433752c4345
Mahdi <mahdibagherichanel@gmail.com>
2025-08-31 21:02:32 +0330

    add ChangeStatusUser

---
+++ server/admin/paneladmin/users/ChangeStatusUser/ChangeStatusUser.ts
@@ -0,0 +1,54 @@
+import prisma from "../../../../../lib/db";
+
+export async function ChangeStatusUserDelete(id: number) {
+  try {
+    const exist = await prisma.ticket.findUnique({
+      where: { id },
+    });
+    if (!exist) {
+      return { error: "چنین تیکتی وجود ندارد", status: 404 };
+    }
+    const change = prisma.user.update({
+      where: { id },
+      data: {
+        status: "Blocked",
+      },
+    });
+    return {
+      message: "وضعیت کاربر با موفقیت تغییر کرد",
+      user: change,
+      status: 200,
+    };
+  } catch {
+    return {
+      error: "مشکلی در تغییر وضعیت کاربر رخ داده است",
+      status: 500,
+    };
+  }
+}
+export async function ChangeStatusUserVrify(id: number) {
+  try {
+    const exist = await prisma.ticket.findUnique({
+      where: { id },
+    });
+    if (!exist) {
+      return { error: "چنین تیکتی وجود ندارد", status: 404 };
+    }
+    const change = prisma.user.update({
+      where: { id },
+      data: {
+        status: "Approved",
+      },
+    });
+    return {
+      message: "وضعیت کاربر با موفقیت تغییر کرد",
+      user: change,
+      status: 200,
+    };
+  } catch {
+    return {
+      error: "مشکلی در تغییر وضعیت کاربر رخ داده است",
+      status: 500,
+    };
+  }
+}
