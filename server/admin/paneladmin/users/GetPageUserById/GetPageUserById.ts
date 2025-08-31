a483c82f83dcb9289ff3cf6aa81c4c1ced7c8fdf
Mahdi <mahdibagherichanel@gmail.com>
2025-08-31 20:53:30 +0330

    add GetPageUserById for get user in adminpanel

---
+++ server/admin/paneladmin/users/GetPageUserById/GetPageUserById.ts
@@ -0,0 +1,34 @@
+import prisma from "../../../../../lib/db";
+
+export async function GetPageUserById(userid: number) {
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: userid },
+      select: {
+        id: true,
+        firstname: true,
+        phone: true,
+        email: true,
+        totalMessage: true,
+        report: true,
+        status: true,
+        profileImage: true,
+        ticketsSupport: true,
+        isVerify: true,
+        userRate: true,
+        changephone: true,
+        role: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+
+    if (!user) {
+      return { message: "کاربر وجود نداره !", status: 404 };
+    }
+
+    return { user, status: 200 };
+  } catch {
+    return { error: "مشکلی در سرور رخ داده است", status: 500 };
+  }
+}
