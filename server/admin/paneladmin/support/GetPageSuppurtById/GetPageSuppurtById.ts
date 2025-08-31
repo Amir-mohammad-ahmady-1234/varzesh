4bd71f446f798a757874451b7878acb767c501e4
Mahdi <mahdibagherichanel@gmail.com>
2025-08-31 10:32:19 +0330

    add GetPageSuppurtById

---
+++ server/admin/paneladmin/support/GetPageSuppurtById/GetPageSuppurtById.ts
@@ -0,0 +1,19 @@
+import prisma from "../../../../../lib/db";
+
+export async function GetPageSuppurtById(id: number) {
+  try {
+    const existsuppurtid = await prisma.ticket.findUnique({
+      where: { id },
+      include: {
+        user: true,
+        messages: true,
+      },
+    });
+    if (!existsuppurtid) {
+      return { error: "چنین تیکتی وجود ندارد", status: 404 };
+    }
+    return { success: true, ticket: existsuppurtid };
+  } catch {
+    return { error: "مشکلی در سرور رخ داده است", status: 500 };
+  }
+}
