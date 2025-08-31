182bbcb706edb9314ac4d60dfe1a46d68a0b80dc
Mahdi <mahdibagherichanel@gmail.com>
2025-08-31 09:23:08 +0330

    add image url and model ticket

--- components/pages/auth/SubmitButton.tsx
+++ components/pages/auth/SubmitButton.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import React from "react";
 import OtpTimer from "./resetPassForm/OtpTimer";
-import Button from "../../../styles/ui/Button";
+import Button from "../../common/Button";
 
 function SubmitButton({
   children,
