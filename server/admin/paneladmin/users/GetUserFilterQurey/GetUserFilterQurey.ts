74b13acea8a67fd6d7aa7a99f1c95f805fd843b7
Mahdi <mahdibagherichanel@gmail.com>
2025-09-18 23:27:32 +0330

    add pagination for user filter

--- server/admin/paneladmin/users/GetUserFilterQurey/GetUserFilterQurey.ts
+++ server/admin/paneladmin/users/GetUserFilterQurey/GetUserFilterQurey.ts
@@ -5,11 +5,15 @@ interface IUserFilter {
   status?: "Blocked" | "Waiting" | "Approved";
   role?: "USER" | "ADMIN";
   search?: string;
+  page?: number;
+  limit?: number;
 }
 
 export async function GetUserFilterQuery({
   status,
   role,
+  page = 1,
+  limit = 10,
   search,
 }: IUserFilter) {
   try {
@@ -28,11 +32,26 @@ export async function GetUserFilterQuery({
         { phone: { contains: serchietms } },
       ];
     }
-    const users = await prisma.user.findMany({
-      where,
-      orderBy: { createdAt: "desc" },
-    });
-    return { status: 200, users };
+    const skip = (page - 1) * limit;
+    const [users, total] = await Promise.all([
+      await prisma.user.findMany({
+        where,
+        skip,
+        take: limit,
+        orderBy: { createdAt: "desc" },
+      }),
+      prisma.user.count({ where }),
+    ]);
+
+    return {
+      data: users,
+      pagination: {
+        total,
+        page,
+        limit,
+        totalPages: Math.ceil(total / limit),
+      },
+    };
   } catch (err) {
     console.error(err);
     return { error: "مشکلی در سرور رخ داده است", status: 500 };
