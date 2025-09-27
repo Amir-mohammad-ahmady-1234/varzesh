59aae2ae9c0b62aaefce3c5fc8b4ea4c6c201d65
Mahdi <mahdibagherichanel@gmail.com>
2025-09-27 17:29:27 +0330

    change logic in create vlog

--- server/admin/paneladmin/blog/BlogCreate.ts
+++ server/admin/paneladmin/blog/BlogCreate.ts
@@ -1,18 +1,25 @@
 import prisma from "../../../../lib/db";
+import { uploadFile } from "../../../../utils/uploadFile";
 
 export type BlogProps = {
   id?: number;
   title: string;
-  profile: string;
-  img: string;
+  profile: File;
+  img: File;
   category: string;
   summary: string;
   description: string;
 };
 
 export async function BlogCreate(props: BlogProps) {
+  const dbPath = await uploadFile(props.img, "uploads");
+  const dbPathprofile = await uploadFile(props.profile, "uploads");
   const blog = await prisma.blog.create({
-    data: props,
+    data: {
+      ...props,
+      img: dbPath,
+      profile: dbPathprofile,
+    },
   });
   return blog;
 }
