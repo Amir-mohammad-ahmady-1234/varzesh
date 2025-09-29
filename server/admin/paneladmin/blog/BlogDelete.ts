377f342ffd62afe43b774b4cf0962a16c8ac9728
Amir-Mohammad <amyrmhmdahmdy179@gmail.com>
2025-09-29 16:53:12 -0700

    .

---
+++ server/admin/paneladmin/blog/BlogDelete.ts
@@ -0,0 +1,12 @@
+import prisma from "../../../../lib/db";
+
+type BlogDeleteProps = {
+  id: number;
+};
+
+export async function BlogDelete(props: BlogDeleteProps) {
+  const blog = await prisma.blog.delete({
+    where: { id: props.id },
+  });
+  return blog;
+}
---
+++ server/user/paneluser/blog/BlogByIdUser.ts
@@ -0,0 +1,6 @@
+import prisma from "../../../../lib/db";
+
+export async function BlogByIdUser(id: number) {
+  const blog = await prisma.blog.findUnique({ where: { id } });
+  return blog;
+}
