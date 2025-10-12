39ec426aa40bdf2f473a6d636dd48f41bbd779c5
Amir-Mohammad <amyrmhmdahmdy179@gmail.com>
2025-10-11 22:21:49 -0700

    add news page

--- components/pages/home/news/LastNews/LastNews.tsx
+++ components/pages/home/news/LastNews/LastNews.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image";
+
 import React from "react";
 import { GetLastNews } from "../../../../../server/user/paneluser/news/GetLastNews";
 import { $Enums } from "@prisma/client";
+import Link from "next/link";
 
 interface NewsType {
   id: number;
@@ -19,22 +21,21 @@ async function LastNews() {
   return (
     <div className="flex flex-col p-4 md:p-6 space-y-4 bg-tertiary-300 rounded-md">
       {news.map((item) => (
-        <div
-          key={item.id}
-          className="flex flex-col sm:flex-row items-start sm:items-center gap-3"
-        >
-          <Image
-            className="rounded-md flex-shrink-0"
-            width={70}
-            height={70}
-            src={item.img}
-            alt={item.title}
-          />
-          <div className="flex flex-col">
-            <p className="text-md font-semibold">{item.title}</p>
-            <p className="text-sm text-neutral-600">{item.description}</p>
+        <Link href={`/news/${item.id}`} key={item.id}>
+          <div className="flex flex-col sm:flex-row items-start sm:items-center gap-3">
+            <Image
+              className="rounded-md flex-shrink-0"
+              width={70}
+              height={70}
+              src={item.img}
+              alt={item.title}
+            />
+            <div className="flex flex-col">
+              <p className="text-md font-semibold">{item.title}</p>
+              <p className="text-sm text-neutral-600">{item.description}</p>
+            </div>
           </div>
-        </div>
+        </Link>
       ))}
     </div>
   );
